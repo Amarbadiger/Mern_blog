5901b9409a42b5eab65eb158e96160f8a849dcd9
amar <amarbadiger45@gmail.com>
2024-03-06 11:02:27 +0530

    Created Routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
+import Dashboard from "./Pages/Dashboard";
+import Projects from "./Pages/Projects";
 
 const App = () => {
-  return <h1 className="text-3xl text-red-500">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
